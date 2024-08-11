5fd27812ba83d4f8206721a70674163717c67024
shiyu122222 <hiiragiruku@gmail.com>
2024-08-11 20:42:09 +0900

    kadai_021をアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,6 @@
+const pushbtn = document.getElementById('btn');
+const pushtext = document.getElementById('text');
+
+pushbtn.addEventListener('click', () => {
+  setTimeout(() => { pushtext.textContent = 'ボタンをクリックしました'
+  },2000);});
